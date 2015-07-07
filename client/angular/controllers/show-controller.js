57e037559c5e6075cf25aaac8bb75b523907d8a7
bluur <bryantluu22@gmail.com>
2015-07-07 09:33:11 -0700

    added show controller and added claimability

---
+++ client/angular/controllers/show-controller.js
@@ -0,0 +1,79 @@
+myApp.controller('showController', function($scope, eventFactory, $routeParams, $timeout){
+	$scope.events = []; 
+	$scope.items = [];
+	$scope.id = $routeParams.id;
+
+	updateEvents();
+
+	$scope.turnOn = function(item){
+		
+		for (var i in $scope.events[$scope.id].items){
+			if ($scope.events[$scope.id].items[i].name == item){
+				$scope.events[$scope.id].items[i].truth = true;
+			}
+		};
+	}
+
+	$scope.claimItem = function(){
+		console.log($scope.events[$scope.id].items);
+
+	}
+
+	function addItemArray(){
+		console.log($scope.events);
+		for (var i in $scope.events[$scope.id].items){
+			var itemArray = [];
+			for (var j=1; j < $scope.events[$scope.id].items[i].quantity+1; j++){
+				itemArray.push(j);
+			}
+			$scope.events[$scope.id].items[i].itemArray = itemArray;
+		}
+	}
+
+	function updateEvents(){
+		eventFactory.getEvents(function(output){
+			$scope.events = output;
+			addItemArray()
+		})
+	}
+	
+	$scope.addItem = function(){
+		$scope.items.push({
+			name: $scope.newItem.name,
+			quantity: $scope.newItem.quantity
+		});
+
+		$scope.newItem.quantity = "";
+		$scope.newItem.name = "";
+	}
+
+	$scope.addItem2 = function(id){
+		$scope.events[id].items.push({
+			name: $scope.newItem.name,
+			quantity: $scope.newItem.quantity
+		});
+
+		$scope.newItem.quantity = "";
+		$scope.newItem.name = "";
+	}
+
+	$scope.addEvent = function(items){
+		$scope.newEvent.items = items;
+		eventFactory.addEvent($scope.newEvent, function(output){
+			updateEvents();
+			$scope.newEvent = {};
+		})
+	}
+
+	$scope.updateEvent = function(id){
+		$scope.newEvent.items = $scope.events[id].items;
+		eventFactory.updateEvent($scope.newEvent, function(output){
+			updateEvents();
+			$scope.newEvent = {};
+		})
+	}
+
+	$scope.removeItem = function(index){
+
+	}
+})
